04c485100fa5b8147dfbf2d4fc96f13b2905b7f3
Roberto-Orazi <robervgg@gmail.com>
2023-04-14 20:58:51 -0300

    BaSP-W05-2023: Exercise 5 complete

--- week-05/exercises/04-if-else.js
+++ week-05/exercises/04-if-else.js
@@ -1,5 +1,4 @@
 console.log('--EXERCISE 4: IF AND ELSE')
-
 //a. Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than or equal to 0,5” y sino un alerta con el mensaje “Lower than 0,5”.
 console.log('-Exercise 4.a')
 var num = Math.random()
--- week-05/exercises/06-functions.js
+++ week-05/exercises/06-functions.js
@@ -1,5 +1,4 @@
 console.log('--EXERCISE 6: FUNCTIONS')
-
 //a. Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
 console.log('-Exercise 6.a')
 

b7dced79b50381e3c82b607c9c8dca0e8f1b993f
Roberto-Orazi <robervgg@gmail.com>
2023-04-14 17:39:29 -0300

    BaSP-W05-2023: Exercise 1 complete

--- week-05/exercises/01-variables-and-operator.js
+++ week-05/exercises/01-variables-and-operator.js
@@ -1,1 +1,16 @@
 console.log('--EXERCISE 1: VARIABLES AND OPERATORS')
+
+// a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+console.log('-Exercise 1.a')
+var a = 1
+var b = 2
+console.log('La suma de las 2 variables es: ', a + b)
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+var c = 'roberto'
+var d = 'orazi'
+console.log('Mi nombre es ', c + ' ' + d)
+// c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+var e = 'primer'
+var f = 'string'
+var g = e.length + f.length
+console.log('El largo total de caracteres es de: ', g)

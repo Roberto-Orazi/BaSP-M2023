bac739375db652f10202b338d4a8545c3f1311b7
Roberto-Orazi <robervgg@gmail.com>
2023-04-20 12:16:47 -0300

    BaSP-W06-2023: New Folder structure

--- week-05/exercises/05-for.js
+++ week-05/exercises/05-for.js
@@ -7,20 +7,20 @@ for(var i=0; i<5; i++){
 }
 //b. Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
 console.log('-Exercise 5.b')
-for (let i=0; i<arraya.length; i++) {
+for (var i=0; i<arraya.length; i++) {
     console.log(arraya[i] = arraya[i][0].toUpperCase() + arraya[i].substring(1))
 }
 //c. Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
 console.log('-Exercise 5.c')
 var sentence = []
-  for (let i=0; i < arraya.length; i++) {
+  for (var i=0; i < arraya.length; i++) {
     sentence += arraya[i] + ' '
   }
   alert(sentence);
 //d. Crear un array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0 hasta al número 9. Mostrar por la consola del navegador el array final (utilizar console.log).
 console.log('-Exercise 5.d')
 var numeros = []
-for (let i=0; i<10; i++) {
+for (var i=0; i<10; i++) {
     numeros.push(i)
   }
   console.log(numeros)

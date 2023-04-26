17f0fddb8778e0f403865dc19692ec2593c47829
Roberto-Orazi <robervgg@gmail.com>
2023-04-25 23:37:14 -0300

    BaSP-W06-2023: validation and alert finishes

--- week-06/js/sign-up.js
+++ week-06/js/sign-up.js
@@ -1,20 +1,135 @@
-const validation = {
-    name: /^[a-zA-Z\s]{3,40}$/,
-    lastname: /^[a-zA-Z\s]{3,40}$/,
-    id: /^[0-9\s]{7,8}$/,
-	password: /^[a-zA-Z0-9]{8,40}$/, 
-    password2: /^[a-zA-Z0-9]{8,40}$/,
-    phone: /^[0-9\s]{7,8}$/,
-	
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+//Name validation
+const nameField = document.querySelector("[name=name]")
+const validationName = e =>{
+    const field = e.target;
+    const fieldValue  = e.currentTarget.value;
+    const regex = new RegExp(/^[a-zA-Z\s]{3,}$/);
+    if(fieldValue.trim().length > 3 && !regex.test(fieldValue)){
+        field.classList.add("invalid")
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = "Please enter a valid Name with more than 3 characters and only letters";
+    }
+    else{
+        field.classList.remove("invalid")
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
 }
-
+nameField.addEventListener("input", validationName);
+//LastName validation
+const lastNameField = document.querySelector("[name=lastname]")
+const validationLastName = e =>{
+    const field = e.target;
+    const fieldValue  = e.currentTarget.value;
+    const regex = new RegExp(/^[a-zA-Z\s]{3,}$/);
+    if(fieldValue.trim().length > 3 && !regex.test(fieldValue)){
+        field.classList.add("invalid")
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = "Please enter a valid Last Name with more than 3 characters and only letters";
+    }
+    else{
+        field.classList.remove("invalid")
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+}
+lastNameField.addEventListener("input", validationLastName);
+//DNI validation
+const dniField = document.querySelector("[name=dni]")
+const validationDni = e =>{
+    const field = e.target;
+    const fieldValue  = e.currentTarget.value;
+    const regex = new RegExp(/^[0-9]{7,}$/);
+    if(fieldValue.trim().length > 0 && !regex.test(fieldValue)){
+        field.classList.add("invalid")
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = "The document number has to have more than 7 numbers and numbers only";
+    }
+    else{
+        field.classList.remove("invalid")
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+}
+dniField.addEventListener("input", validationDni);
+//PHONE validation
+const phoneField = document.querySelector("[name=phone]")
+const validationPhone = e =>{
+    const field = e.target;
+    const fieldValue  = e.currentTarget.value;
+    const regex = new RegExp(/^[0-9\s]{10,}$/);
+    if(fieldValue.trim().length > 0 && !regex.test(fieldValue)){
+        field.classList.add("invalid")
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = "The Phone number has to have more than 10 numbers";
+    }
+    else{
+        field.classList.remove("invalid")
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+}
+phoneField.addEventListener("input", validationPhone);
+//Address validation
+const addressField = document.querySelector("[name=address]")
+const validationAddress = e =>{
+    const field = e.target;
+    const fieldValue  = e.currentTarget.value;
+    const regex = new RegExp(/^[a-zA-Z0-9]{3,}$/);
+    if(fieldValue.trim().length > 3 && !regex.test(fieldValue)){
+        field.classList.add("invalid")
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = "Please enter a valid city with more than 3 characters and only letters";
+    }
+    else{
+        field.classList.remove("invalid")
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+}
+addressField.addEventListener("input", validationAddress);
+//City validation
+const cityField = document.querySelector("[name=city]")
+const validationCity = e =>{
+    const field = e.target;
+    const fieldValue  = e.currentTarget.value;
+    const regex = new RegExp(/^[a-zA-Z\s]{3,}$/);
+    if(fieldValue.trim().length > 3 && !regex.test(fieldValue)){
+        field.classList.add("invalid")
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = "Please enter a valid city with more than 3 characters and only letters";
+    }
+    else{
+        field.classList.remove("invalid")
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+}
+cityField.addEventListener("input", validationCity);
+//Zip validation
+const zipCodeField = document.querySelector("[name=zipcode]")
+const validationZipCode = e =>{
+    const field = e.target;
+    const fieldValue  = e.currentTarget.value;
+    const regex = new RegExp(/^[0-9]{4,5}$/);
+    if(fieldValue.trim().length > 4 && !regex.test(fieldValue)){
+        field.classList.add("invalid")
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = "Please enter a valid city with more than 3 characters and only letters";
+    }
+    else{
+        field.classList.remove("invalid")
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
+    }
+}
+zipCodeField.addEventListener("input", validationZipCode);
 //Email validation
 const emailsField = document.querySelector("[name=emails]")
 const validationLogin = e =>{
     const field = e.target;
     const fieldValue  = e.currentTarget.value;
-    const regex = new RegExp(/^[a-zA-Z 0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/);
+    const regex = new RegExp(/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/);
     if(fieldValue.trim().length > 4 && !regex.test(fieldValue)){
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
@@ -63,3 +178,4 @@ const validationReppassword = e =>{
     }
 }
 reppasswordField.addEventListener("input", validationReppassword);
+

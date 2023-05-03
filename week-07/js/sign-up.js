b040643e66ad634c6e5af0ecf3a0da64a526b7e1
Roberto-Orazi <robervgg@gmail.com>
2023-05-02 21:05:16 -0300

    BaSP-2023-W07: sign up validation update, now it storage in localstorage the info from the inputs, it still missing the alert

--- week-07/js/sign-up.js
+++ week-07/js/sign-up.js
@@ -1,8 +1,7 @@
-//Name validation
+window.addEventListener("load", () => {
 var nameField = document.querySelector("[name=name]")
-var validationName = e => {
-    var field = e.target;
-    var fieldValue = e.currentTarget.value;
+var validationName = field => {
+    var fieldValue = field.value;
     if (fieldValue.trim().length <= 3) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
@@ -29,12 +28,11 @@ var validationName = e => {
         }
     }
 }
-nameField.addEventListener("input", validationName);
-//Last Name validation
+nameField.addEventListener("input", (e) => validationName(e.target));
+
 var lastNameField = document.querySelector("[name=lastname]")
-var validationLastName = e => {
-    var field = e.target;
-    var fieldValue = e.currentTarget.value;
+var validationLastName = field => {
+    var fieldValue = field.value;
     if (fieldValue.trim().length <= 3) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
@@ -61,12 +59,11 @@ var validationLastName = e => {
         }
     }
 }
-lastNameField.addEventListener("input", validationLastName);
-//DNI validation
+lastNameField.addEventListener("input", (e) => validationLastName(e.target));
+
 var dniField = document.querySelector("[name=dni]")
-var validationDni = e => {
-    var field = e.target;
-    var fieldValue = e.currentTarget.value;
+var validationDni = field => {
+    var fieldValue = field.value;
     if (fieldValue.trim().length <= 7) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
@@ -93,13 +90,11 @@ var validationDni = e => {
         }
     }
 }
-dniField.addEventListener("input", validationDni);
-//PHONE validation
-var
-    phoneField = document.querySelector("[name=phone]")
-var validationPhone = e => {
-    var field = e.target;
-    var fieldValue = e.currentTarget.value;
+dniField.addEventListener("input", (e) => validationDni(e.target));
+
+var phoneField = document.querySelector("[name=phone]")
+var validationPhone = field => {
+    var fieldValue = field.value;
     if (fieldValue.trim().length <= 6) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
@@ -126,16 +121,15 @@ var validationPhone = e => {
         }
     }
 }
-phoneField.addEventListener("input", validationPhone);
-//Address validation
+phoneField.addEventListener("input", (e) => validationPhone(e.target));
+
 var addressField = document.querySelector("[name=address]")
-var validationAddress = e => {
-    var field = e.target;
-    var fieldValue = e.currentTarget.value;
+var validationAddress = field => {
+    var fieldValue = field.value;
     if (fieldValue.trim().length <= 3 || fieldValue.match(/[^a-zA-Z0-9]/)) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
-        field.nextElementSibling.innerText = "Invalid City";
+        field.nextElementSibling.innerText = "Invalid Address";
     }
     else {
         field.classList.remove("invalid")
@@ -144,12 +138,11 @@ var validationAddress = e => {
         //alert(fieldValue);
     }
 }
-addressField.addEventListener("input", validationAddress);
-//City validation
+addressField.addEventListener("input", (e) => validationAddress(e.target));
+
 var cityField = document.querySelector("[name=city]")
-var validationCity = e => {
-    var field = e.target;
-    var fieldValue = e.currentTarget.value;
+var validationCity = field => {
+    var fieldValue = field.value;
     if (fieldValue.trim().length <= 3 || fieldValue.match(/[^a-zA-Z\s]/)) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
@@ -162,12 +155,11 @@ var validationCity = e => {
         //alert(fieldValue);
     }
 }
-cityField.addEventListener("input", validationCity);
-//Zip validation
+cityField.addEventListener("input", (e) => validationCity(e.target));
+
 var zipCodeField = document.querySelector("[name=zipcode]")
-var validationZipCode = e => {
-    var field = e.target;
-    var fieldValue = e.currentTarget.value;
+var validationZipCode = field => {
+    var fieldValue = field.value;
     if (fieldValue.trim().length <= 4 || fieldValue.match(/[^0-9]/)) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
@@ -180,13 +172,12 @@ var validationZipCode = e => {
         //alert(fieldValue);
     }
 }
-zipCodeField.addEventListener("input", validationZipCode);
-//Email validation
+zipCodeField.addEventListener("input", (e) => validationZipCode(e.target));
+
 var emailsField = document.querySelector("[name=emails]")
-var validationLogin = e => {
-    var field = e.target;
-    var fieldValue = e.currentTarget.value;
-     if (fieldValue.trim().length <= 4 || !fieldValue.includes("@") || !fieldValue.includes(".")) {
+var validationLogin = field => {
+    var fieldValue = field.value;
+    if (fieldValue.trim().length <= 4 || !fieldValue.includes("@") || !fieldValue.includes(".")) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
         field.nextElementSibling.innerText = "Invalid Email";
@@ -195,15 +186,15 @@ var validationLogin = e => {
         field.classList.remove("invalid")
         field.nextElementSibling.classList.remove("error");
         field.nextElementSibling.innerText = "";
-        alert(fieldValue);
+        //alert(fieldValue);
     }
 }
-emailsField.addEventListener("input", validationLogin);
-//Password validation
+emailsField.addEventListener("input", (e) => validationLogin(e.target));
+
+
 var passwordField = document.querySelector("[name=password]")
-var validationPassword = e => {
-    var field = e.target;
-    var fieldValue = e.currentTarget.value;
+var validationPassword = field => {
+    var fieldValue = field.value;
     if (fieldValue.trim().length <= 8 || fieldValue.match(/[^a-zA-Z0-9]/)) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
@@ -216,12 +207,11 @@ var validationPassword = e => {
         //alert(fieldValue);
     }
 }
-passwordField.addEventListener("input", validationPassword);
-//Repeat Password validation
+passwordField.addEventListener("input", (e) => validationPassword(e.target));
+
 var reppasswordField = document.querySelector("[name=reppassword]")
-var validationReppassword = e => {
-    var field = e.target;
-    var fieldValue = e.currentTarget.value;
+var validationReppassword = field => {
+    var fieldValue = field.value;
     if (fieldValue.trim().length <= 8 || fieldValue.match(/[^a-zA-Z0-9]/)) {
         field.classList.add("invalid")
         field.nextElementSibling.classList.add("error");
@@ -243,78 +233,88 @@ var validationReppassword = e => {
         }
     }
 }
-reppasswordField.addEventListener("input", validationReppassword);
-//SUBMIT VALIDATION
+reppasswordField.addEventListener("input", (e) => validationReppassword(e.target));
+
+var dobField = document.querySelector("[name=dob]")
+   
+
+
 var submitButton = document.querySelector('button[type="submit"]');
-submitButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  var url =
-    "https://api-rest-server.vercel.app/signup?" +
-    "name=" +
-    nameField.value +
-    "&lastName=" +
-    lastNameField.value +
-    "&dni=" +
-    dniField.value +
-    "&phone=" +
-    phoneField.value +
-    "&address=" +
-    addressField.value +
-    "&zip=" +
-    zipCodeField.value +
-    "&city=" +
-    cityField.value +
-    "&email=" +
-    emailsField.value +
-    "&password=" +
-    passwordField.value +
-  event.preventDefault();
-  if (
-    validationLogin(emailsField.value) &&
-    validationName(nameField.value) &&
-    validationLastName(lastNameField.value) &&
-    validationDni(dniField.value) &&
-    validationPhone(phoneField.value) &&
-    validationAddress(addressField.value) &&
-    validationCity(cityField.value) &&
-    validationZipCode(zipCodeField.value) &&
-    validationPassword(passwordField.value)
-  ) {
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    var url =
+        "https://api-rest-server.vercel.app/signup?" +
+        "name=" +
+        nameField.value +
+        "&lastName=" +
+        lastNameField.value +
+        "&dni=" +
+        dniField.value +
+        "&dob"+
+        dobField.value +
+        "&phone=" +
+        phoneField.value +
+        "&address=" +
+        addressField.value +
+        "&zip=" +
+        zipCodeField.value +
+        "&city=" +
+        cityField.value +
+        "&email=" +
+        emailsField.value +
+        "&password=" +
+        passwordField.value;
+
     fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        if (data.hasOwnProperty("data")) {
-          var keys = Object.keys(data.data);
-          for (var i = 1; i < keys.length; i++) {
-            var key = keys[i];
-            if (data.data.hasOwnProperty(key)) {
-              var value = data.data[key];
-              localStorage.setItem(key, value);
-              alert(key + ": " + value);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            if (data.success) {  
+                localStorage.setItem("name", nameField.value);
+                localStorage.setItem("lastName", lastNameField.value);
+                localStorage.setItem("address", addressField.value);
+                localStorage.setItem("zip", zipCodeField.value);
+                localStorage.setItem("phone", phoneField.value);
+                localStorage.setItem("dni", dniField.value);
+                localStorage.setItem("dob", dobField.value);
+                localStorage.setItem("city", cityField.value);
+                localStorage.setItem("email", emailsField.value);
+                localStorage.setItem("password", passwordField.value);
+                return alert(data.msg);
             }
-          }
-        } else if (data.hasOwnProperty("errors")) {
-          for (var i = 0; i < data.errors.length; i++) {
-            var error = data.errors[i];
-            if (error.hasOwnProperty("msg")) {
-              alert(error.msg);
+            if(data.error){
+                var errorsFetch;
+                for(var i = 0; i < data.errors.length; i++){
+                    "The " + 
+                    data.errors[i].param +
+                    ": " +
+                    data.errors[i].value; +
+                    "\n has this error: " +
+                    data.errors[i].msg;  
+                }
+                throw new Error(errorsFetch);
             }
-          }
+        })
+        .catch(function(error) {
+            alert(error);
+        });
+        
+        getInfo();
+        
+        function getInfo() {
+            nameField.value = localStorage.getItem("name");
+            lastNameField.value = localStorage.getItem("lastName");
+            addressField.value = localStorage.getItem("address");
+            zipCodeField.value = localStorage.getItem("zip");
+            phoneField.value = localStorage.getItem("phone");
+            dniField.value = localStorage.getItem("dni");
+            dobField.value = localStorage.getItem("dob");
+            cityField.value = localStorage.getItem("city");
+            emailsField.value = localStorage.getItem("email");
+            passwordField.value = localStorage.getItem("password");
+            reppasswordField.value = localStorage.getItem("password");
         }
-      });
-  }
-});
-document.addEventListener("DOMContentLoaded", ReloadInfo());
-function ReloadInfo() {
-  nameField.value = localStorage.getItem("name");
-  lastNameField.value = localStorage.getItem("lastName");
-  addressField.value = localStorage.getItem("address");
-  zipCodeField.value = localStorage.getItem("zip");
-  phoneField.value = localStorage.getItem("phone");
-  dniField.value = localStorage.getItem("dni");
-  cityField.value = localStorage.getItem("city");
-  emailsField.value = localStorage.getItem("email");
-  passwordField.value = localStorage.getItem("password");
-}
+
+})
+})
